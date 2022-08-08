72cad8f83badbc94fc7842648543adbbc48eb537
Kevin Tan <kevintanhongann@gmail.com>
2022-08-08 19:20:16 +0800

    remove unused

--- src/main/java/xyz/doubleedgetech/web/rest/errors/handlers/UnsatisfiedRouteExceptionHandler.java
+++ src/main/java/xyz/doubleedgetech/web/rest/errors/handlers/UnsatisfiedRouteExceptionHandler.java
@@ -7,7 +7,6 @@ import io.micronaut.context.annotation.Replaces;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.server.exceptions.ExceptionHandler;
-//import io.micronaut.http.server.netty.converters.UnsatisfiedRouteHandler;
 import io.micronaut.web.router.exceptions.UnsatisfiedRouteException;
 import org.zalando.problem.Problem;
 import org.zalando.problem.Status;
